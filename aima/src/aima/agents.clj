32d6cefad10fb764ad10cae1272d4580fa0f518a
alanmarazzi <alan.marazzi@gmail.com>
2018-09-08 19:28:56 +0200

    Start Agents with Clojure

--- aima/src/aima/agents.clj
+++ aima/src/aima/agents.clj
@@ -5,7 +5,9 @@
   To personalize it `swap!` and `assoc` the atom"
   [& [name]]
   (atom {:alive false
-         :name name}))
+         :name name
+         :location nil
+         :type :things}))
 
 (defn new-agent
   "Creates a new agent"
@@ -15,6 +17,80 @@
          :bump false
          :holding []
          :performance 0
-         :program program}))
+         :program program
+         :location nil
+         :type :agents}))
 
+(defn new-watcher
+  [a k h]
+  (add-watch a k
+             (fn [k a o n]
+               (println "=== CHANGE ===")
+               (println (str k ": "))
+               (println o)
+               (println n)
+               (newline)
+               (swap! h update k conj o))))
 
+(defn alive?
+  [obj]
+  (:alive @obj))
+
+(defn new-environment
+  [objs agents]
+  (atom {:things objs
+         :agents agents}))
+
+(defn perceive-&-run
+  [agent percept]
+  (let [f (:program @agent)]
+    (when (alive? agent)
+      (f (percept agent)))))
+
+(defn step
+  [env done? execute]
+  (when-not (done? env)
+    (let [actions (map perceive-&-run (:agents @env))]
+      (map execute actions))))
+
+(defn same-location?
+  [obj location]
+  (= (:location @obj)
+     location))
+
+(defn same-name?
+  [obj name-compare]
+  (= (:name @obj)
+     name-compare))
+
+; I don't care for types at the moment
+(defn list-things
+  ([env location]
+   (let [things (:things @env)]
+     (filter #(same-location? % location) things)))
+  ([env location kind]
+   (let [things (list-things env location)]
+     (filter #(= (:name %) kind) things))))
+
+(defn location-empty?
+  [env location]
+  (empty? (list-things env location)))
+
+(defn set-location
+  [obj location]
+  (swap! obj assoc :location location))
+
+(defn add-thing
+  [env thing location]
+  (let [t (:type @thing)]
+    (swap! thing assoc :location location)
+    (swap! env update t conj thing)))
+
+(defn remove-thing
+  [env thing]
+  (let [t (:type @thing)]
+    (swap! env #(remove #{thing} (t %)))))
+
+(defn done?
+  [env & preds]
+  ((apply every-pred preds) @env))
--- aima/src/aima/core.clj
+++ aima/src/aima/core.clj
@@ -1,6 +1,56 @@
-(ns aima.core)
+(ns aima.core
+  (:require [aima.agents :refer :all :reload true]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; Blind dog example
+(def dog
+  (new-agent nil "blind-dog"))
+
+(def food (new-object "food"))
+
+(def water (new-object "water"))
+
+(def park
+  (new-environment))
+
+(defn percept
+  [env agent]
+  (list-things env (:location @agent)))
+
+(defn move-down
+  [agent]
+  (swap! agent update :location inc))
+
+(defn consume-thing
+  [env agent kind]
+  (let [things (list-things env (:location @agent) kind)]
+    (when (not-empty things)
+      (remove-thing env (first things))
+      things)))
+
+(defn eat
+  [env agent]
+  (consume-thing env agent "food"))
+
+(defn drink
+  [env agent]
+  (consume-thing env agent "water"))
+
+(defn execute
+  [env agent action]
+  (case action
+    "move-down" (do (println (:name @agent) "decided to" action "at" (:location @agent))
+                    (move-down agent))
+    "eat" (println (:name @agent) "ate" (first (eat env agent)) "at" (:location @agent))
+    "drink" (println (:name @agent) "drank" (first (drink env agent)) "at" (:location @agent))))
+
+(defn check-alive?
+  [env]
+  (not (some alive? (:agents @env))))
+
+(defn check-edible?
+  [env]
+  (not (some #{"food" "water"} (:things @env))))
+
+(defn program
+  [percepts]
+  )
